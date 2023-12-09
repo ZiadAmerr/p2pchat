7846d5901a2a92385590e7cd05021823adda85e1
MohamedGira <mohamedgira0901@gmail.com>
2023-12-09 23:45:00 +0200

    refactoring, renaming

--- p2pchat/protocols/tcp_request_transceiver.py
+++ p2pchat/protocols/tcp_request_transceiver.py
@@ -1,9 +1,9 @@
 import p2pchat.data as data
 import pickle
-
-class RequestTransceiver:
+import logging
+class TCPRequestTransceiver:
     """
-    this is intended to be a class that wraps the socket.recv/socket.send functions
+    this is intended to be a class that wraps the socket.recv/socket.send TCP functions 
     it will be used to:
     1-send and recieve requests/responses to/from  the server
     
@@ -12,15 +12,16 @@ class RequestTransceiver:
     send_message(message:dict)->None
     recieve_message()->dict or None
     """
+    header_size=data.header_size
+    packet_size=max(20,header_size)
     def __init__(self,connection):
         self.connection=connection
         self.type=None
         self.message=None
-        self.header_size=data.header_size
-        self.packet_size=max(20,self.header_size)
         self.full_message=None
         self.new_message=True
         self.message_length=None
+
 
     def _gather_message(self,dest=None,message=None):
         if message is None:
@@ -32,28 +33,39 @@ class RequestTransceiver:
         return dest
 
     def recieve_message(self)->dict:
-        while True:
-            message = self.connection.recv(self.packet_size)
-            if(self.new_message and message):
-                self.message_length=int(message[:self.header_size])
+        #TODO: add a timeout
+        try:
+            if self.new_message:
+                header = self.connection.recv(self.header_size)
+                if(not len(header)):
+                    return None
+                self.message_length=int(header)
                 self.new_message=False
-            self.full_message=self._gather_message(self.full_message,message)
-            print(len(self.full_message),self.message_length)
-            if len(self.full_message)-self.header_size==self.message_length:
-                self.message=pickle.loads(self.full_message[self.header_size:])
-                break
-        self.full_message=None
-        self.new_message=True
-        return self.message
-
+            while True:
+                self.full_message=self._gather_message(self.full_message,self.connection.recv(self.packet_size))
+                if(len(self.full_message)>self.message_length):
+                    logging.WARN("hmmm, check this out")
+                if len(self.full_message)==self.message_length:
+                    self.message=pickle.loads(self.full_message)
+                    break
+            self.full_message=None
+            self.new_message=True
+            return {"header":header ,"body":self.message}
+        except Exception as e:
+            print("Connection broken: ",e)
+            return None
+    
+    
     def send_message(self,message:dict):
         message_in_bytes=pickle.dumps(message)
         self.connection.send(self._add_header(message_in_bytes))
 
-    def _add_header(self,message:bytes)->bytes:
+    @staticmethod
+    def _add_header(message:bytes)->bytes:
+        #TODO: move to utils or common class
         """
         adds any necessary information to the message header, for example, message size
         need to address what headers are important and how they are formatted, otherwise we will need to add more bytes for a config-file-like headers
         """
-        message_size=f'{len(message):<{self.header_size}}'
+        message_size=f'{len(message):<{TCPRequestTransceiver.header_size}}'
         return bytes(message_size,'utf-8')+message
