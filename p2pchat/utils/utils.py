6d7be90f11d53b276e7a488594650d9d8989a92d
MohamedGira <mohamedgira0901@gmail.com>
2023-12-09 23:42:05 +0200

    added different utils for the app

---
+++ p2pchat/utils/utils.py
@@ -0,0 +1,30 @@
+import datetime
+import time 
+import uuid
+import sys
+def get_timesamp():
+    """ unix timestamp"""
+    return time.mktime(datetime.datetime.now().timetuple())
+def to_human_time(timestamp):
+    """ user-friendly format"""
+    return datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+def get_unique_id():
+    return str(uuid.uuid4())
+def validate_request(requset, required_fields):
+    """ check if request contains all required fields"""
+    for field in required_fields:
+        if field not in requset:
+            return False
+    return True
+
+def dict_factory(cursor, row):
+    """ by default sqlite return qurey results as tuples,
+      we override the connection.row_factory to convert sqlite3 rows to dicts for ease of access"""
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+def sigint_handler(signal, frame):
+    print("\nCtrl+C detected. Closing the socket and exiting.")
+    sys.exit(0)
