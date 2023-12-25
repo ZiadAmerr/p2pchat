91d7726b10afe1d85f3f8ff7e499a229ec18fb52
MohamedGira <mohamedgira0901@gmail.com>
2023-12-26 00:07:39 +0200

    integrated one to one chat into the main app

--- p2pchat/cli.py
+++ p2pchat/cli.py
@@ -1,49 +1,61 @@
 import __init__
-from p2pchat.peer.peer_client import Client
-import re
+from p2pchat.peer.peer_client import *
+from p2pchat.peer.peer_server import *
+
 import time
 import uuid  # Import the UUID module
 from p2pchat.custom_logger import app_logger
 import logging
-logging.basicConfig(level=logging.DEBUG)
+from p2pchat.utils.colors import *
+from p2pchat.utils.utils import clear_console
+from p2pchat.utils.chat_histoty import history,print_and_remember
+from p2pchat.globals import not_chatting
+logging.basicConfig(level=logging.INFO)
+
 class App:
     def __init__(self):
-        self.client=Client()
+        self.client_auth=ClientAuth()
         self.incorrect_attempt = {"incorrect_attempt": 0}
-    
+        self.active_peers=[]
+        self.peer_server=PeerServer()
+        self.peer_client=PeerClient()
+
     def welcome_state(self):
         print("Welcome to Our chat")
         print()
         has_account = input("Do you have an account? (y/n): ")
 
         while has_account not in ['y','n']:
-            print("Please enter a valid response")
+            print(yellow_text("Please enter a valid response"))
             has_account = input("Do you have an account? (y/n): ")
         if has_account == "y":
             return "Login"
         elif has_account == "n":
             return "Sign Up"
 
-
     def login_state(self):
+        if self.client_auth.user is not None:
+            return "Main Menu"
+        
         if "incorrect_attempt" in self.incorrect_attempt and self.incorrect_attempt["incorrect_attempt"] >= 3:
-            print("Account locked. Try again in 1 minute.")
+            print(red_text("Account locked. Try again in 1 minute."))
             return None
 
         if "incorrect_attempt" in self.incorrect_attempt and self.incorrect_attempt["incorrect_attempt"]:
-            print("Incorrect username or password")
+            print(red_text("Incorrect username or password"))
             print()
 
         username = input("Username: ")
         password = input("Password: ")
 
-        if len(password) < 6:
-            print("Invalid password. Password must be at least 6 digits long.")
+        """ if len(password) < 6:
+            print(red_text("Invalid password. Password must be at least 6 digits long."))
             return "Login"
-        
-        response=self.client.login(username, password)
-        app_logger.debug(response)
-        if response.get("body").get("is_success") == True:
+         """
+        response=self.client_auth.login(username, password, self.peer_server.tcp_manager.port,self.peer_server.udp_manager.port)
+        if response.get("body",{}).get("is_success"):
+            self.peer_server.start()
+            self.peer_server.set_user(self.client_auth.user)
             self.incorrect_attempt["incorrect_attempt"] = 0
             return "Main Menu"
         else:
@@ -60,13 +72,13 @@ class App:
             print("Invalid email address.")
             return "Sign Up" """
 
-        if len(password) < 6:
+        """ if len(password) < 6:
             print("Invalid password. Password must be at least 6 digits long.")
-            return "Sign Up"
-        response=self.client.signup(username,password)
+            return "Sign Up" """
+        response=self.client_auth.signup(username,password)
         app_logger.debug(response)
-        if response.get("body").get("is_success") == False:
-            print(response.get("body").get("message"))
+        if response.get("body",{}).get("is_success") == False:
+            print(response.get("body",{}).get("message"))
             return "Sign Up"
         
 
@@ -82,6 +94,7 @@ class App:
         print("4. Join Available Rooms")
         print("5. List online users")
         print("6. Exit")
+        
 
         choice = input("Please enter your choice: ")
         if choice == "1":
@@ -96,7 +109,9 @@ class App:
             return "list"
         elif choice =="6":
             return "exit"
-
+        else:
+            return "Main Menu"
+        
     def show_your_profile_state(self):
         print("Your Profile")
 
@@ -129,7 +144,6 @@ class App:
                 print("Please sign up to create your profile.")
 
         return "Main Menu"
-
     #YAGNI
     def show_others_profile_state(self):
         print("Show Others' Profile")
@@ -146,21 +160,27 @@ class App:
 
         return "Main Menu"
 
-
     def send_msg_state(self):
         recipient_username = input("Enter the username of the recipient: ")
-        if recipient_username not in usernames:
-            print("User with username", recipient_username, "not found.")
+        if recipient_username not in [user["username"] for user in self.active_peers] :
+            self.active_peers=self.client_auth.get_online_peers()
+            if recipient_username not in [user["username"] for user in self.active_peers] :
+                print("User with username", recipient_username, "is either offline or not found.")
+                return "Main Menu"
+        user=next(user for user in self.active_peers if user["username"]==recipient_username)
+        self.recipient=user
+        history.reset_history()
+        response=self.peer_client.enter_chat(self.client_auth.user,user)
+        if response and response.get('body').get('code') ==52 : #shof tare2a a7san
+            key=response.get('body').get('data').get('key')
+            
+            self.peer_server.setup_chat(key)
+            print_and_remember(blue_text("you are the client"))
+            self.peer_client.chat(self.client_auth.user,user,key)
+            self.peer_server.end_chat()
         else:
-            message = input("Enter your message: ")
-            print("Message sent successfully!")
-        
-            if "communicated_with" not in self.state_data['user']:
-                self.state_data['user']['communicated_with'] = set()
-
-            self.state_data['user']['communicated_with'].add(recipient_username)
-
-        return "Main Menu" 
+            print(red_text("couldn't connect"))
+        return "Main Menu"
 
     def join_room_state(self):
         available_rooms = ["General", "Sports", " politics"]
@@ -198,28 +218,29 @@ class App:
         return "Main Menu"
 
     def list_state(self):
-        online_users = ["hanna", "ziad", "gira"]  
+        
+        online_users = self.client_auth.get_online_peers()
         print("Online Users:")
-        if online_users:
+        if len(online_users):
+            self.active_peers=online_users
             for user in online_users:
-                print(f"{user}")
+                print(f"- {user['username']}")
         else:
             print("No users are currently online.")
-
+        print()
         return "Main Menu"
 
     def exit_state(self):
-        self.client.logout()
+        self.client_auth.logout()
         print("Goodbye!")
         return "Welcome"
-
-
 
 
 
     def main(self):
         state = "Welcome"
         while True:
+            not_chatting.wait()
             if state == "Welcome":
                 next_state = self.welcome_state()
             elif state == "Login":
@@ -240,13 +261,13 @@ class App:
                     next_state= self.list_state()
                 elif next_state =="exit":
                     next_state= self.exit_state()
-            
-        
             elif next_state is None:
-                app_logger.debug("next_state is None")
-                time.sleep(60)
-                self.incorrect_attempt["incorrect_attempt"] = 0
-                next_state = "Login"
+                if self.client_auth.user is not None:
+                    next_state="Main Menu"
+                else:
+                    time.sleep(60)
+                    self.incorrect_attempt["incorrect_attempt"] = 0
+                    next_state = "Login"
 
             state = next_state
 
@@ -254,3 +275,5 @@ class App:
 if __name__ == "__main__":
     my_app= App()
     my_app.main()
+    
+    
