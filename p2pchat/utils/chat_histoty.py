c8072b6ae7ba6e90ce713d26b20af0212df5eaf1
MohamedGira <mohamedgira0901@gmail.com>
2023-12-27 07:57:40 +0200

    devise a chat utility to print volatile messages (notifications)

--- p2pchat/utils/chat_histoty.py
+++ p2pchat/utils/chat_histoty.py
@@ -1,11 +1,17 @@
 import __init__
 from p2pchat.utils.utils import clear_console
+
 def print_and_remember(*args,ending_line=None):
     text = ' '.join([str(arg) for arg in args])
     history.append(text)
     clear_console()
     history.print_chat(ending_line)
 
+def print_volatile_message(*args,ending_line=None):
+    text = ' '.join([str(arg) for arg in args])
+    history.append((text,'volatile'))
+    clear_console()
+    history.print_chat(ending_line)
 
 class History:
     def __init__(self):
@@ -16,8 +22,10 @@ class History:
         self.history=[]
     def print_chat(self,ending_line=None):
         for msg in self.history:
-            print(msg)
+            print(msg if isinstance(msg,str) else msg[0])
         if ending_line:
             print(ending_line,end="")
+    def clear_volatile_messages(self):
+        self.history=[msg for msg in self.history if isinstance(msg,str) or msg[1]!='volatile']
 history=History()
 
