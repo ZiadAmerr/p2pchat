df03d235758cca8fa523d0a74726f87eeb8c880a
MohamedGira <mohamedgira0901@gmail.com>
2023-12-27 07:56:59 +0200

    added another event

--- p2pchat/globals.py
+++ p2pchat/globals.py
@@ -4,9 +4,12 @@ import threading
 peer_left=threading.Event()
 not_chatting=threading.Event()
 is_in_chat=threading.Event()
+ignore_input=threading.Event()
+
 not_chatting.set()
 peer_left.clear()
 is_in_chat.clear()
+ignore_input.clear()
 """
 peer_left:      when server detects peer is left, it sets this event,
                 so that we can ignore next input from this client and go to main ment (buffer to ignore input)
@@ -14,4 +17,7 @@ peer_left:      when server detects peer is left, it sets this event,
 not_chatting:   used by the main loop to continue or pause
 
 is_in_chat  :   used to reject reqeusts if already in private chat
+
+ignore_input:   used whenever you wish your current request to inturrpt others. for example, if u are chatting and somone wished to join the room,
+                you can set this event in the server thread, and check it in chatting thread to ignore the input
 """
