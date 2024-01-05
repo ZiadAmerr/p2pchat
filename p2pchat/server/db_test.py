7bb6ae766fe55b3d34fc63106cfaa6cfbf4147e7
MohamedGira <mohamedgira0901@gmail.com>
2024-01-05 09:58:29 +0200

    base for performance testing

---
+++ p2pchat/server/db_test.py
@@ -0,0 +1,31 @@
+from server_db import ServerDB,SQLiteConnectionPool
+import threading,time
+import datetime 
+import matplotlib.pyplot as plt
+
+# demo
+if __name__ == "__main__":
+    db = ServerDB(10)
+   
+    x=[]
+    y=[]
+    for i in range(1,1000,10):
+            
+        threads = []
+        for _ in range(i):
+            t=threading.Thread(target=db.get_chatroom_members,args=("bb",))
+            threads.append(t)
+        start_time = time.time()
+        for thread in threads:
+            thread.start()
+        for thread in threads:
+            thread.join()
+
+        # End time
+        end_time = time.time()
+        elapsed_time_millis = (end_time - start_time) * 1000
+        print(f"Elapsed time for {i} threads: {elapsed_time_millis:.2f} milliseconds")
+        x.append(i)
+        y.append(elapsed_time_millis)
+    plt.plot(x,y)
+    plt.show()
